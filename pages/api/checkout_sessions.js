0345346efde94e7faf95b3aed47d4fddff884d80
tzztson <zhenghu61919@gmail.com>
2022-11-30 05:19:17 +0800

    intial commit

---
+++ pages/api/checkout_sessions.js
@@ -0,0 +1,32 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: "LLC",
+              },
+              unit_amount: 150,
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      });
+      res.redirect(303, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
